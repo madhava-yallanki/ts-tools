ac42e2477a2d0164faf23ec8c86ee9273c9e9c4a
Madhava Yallanki <madhava@yallanki.dev>
2025-02-26 12:00:42 -0800

    Made files field configurable

--- lib/index.ts
+++ lib/index.ts
@@ -3,26 +3,32 @@ import tseslint from "typescript-eslint";
 import prettierPlugin from "eslint-plugin-prettier/recommended";
 import importPlugin from "eslint-plugin-import";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  prettierPlugin,
-  importPlugin.flatConfigs.recommended,
-  {
-    files: ["lib/**/*.ts"],
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-        ecmaVersion: "latest",
-        sourceType: "module",
+type ConfigArgs = {
+  files: string[];
+};
+
+export function eslintConfig({ files }: ConfigArgs) {
+  return tseslint.config(
+    eslint.configs.recommended,
+    ...tseslint.configs.recommendedTypeChecked,
+    prettierPlugin,
+    importPlugin.flatConfigs.recommended,
+    {
+      files: files,
+      languageOptions: {
+        parser: tseslint.parser,
+        parserOptions: {
+          projectService: true,
+          tsconfigRootDir: import.meta.dirname,
+          ecmaVersion: "latest",
+          sourceType: "module",
+        },
       },
-    },
-    rules: {
-      "import/extensions": ["error", "ignorePackages"],
-      "import/no-unresolved": "off",
-      "@typescript-eslint/require-await": "off",
-    },
-  }
-);
+      rules: {
+        "import/extensions": ["error", "ignorePackages"],
+        "import/no-unresolved": "off",
+        "@typescript-eslint/require-await": "off",
+      },
+    }
+  );
+}
