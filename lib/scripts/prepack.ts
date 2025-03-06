e896113023c5e8e600194881f2c60322442804a8
Madhava Yallanki <madhava@yallanki.dev>
2025-03-06 14:18:31 -0800

    Fixed package root derivation in pre-pack script

--- lib/scripts/prepack.ts
+++ lib/scripts/prepack.ts
@@ -1,16 +1,14 @@
 import { readFileSync, writeFileSync } from "fs";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+import { join } from "path";
 
-function updateBundleDependencies(args: string[]) {
-  console.log({ args }, "Update bundle dependencies invoked.");
-  const [rootDir] = args;
-  const packageRoot = dirname(fileURLToPath(rootDir));
-  const filePath = join(packageRoot, `../package.json`);
+function updateBundleDependencies() {
+  const filePath = join(process.cwd(), `package.json`);
+  console.log({ filePath }, "Update bundle dependencies invoked.");
+
   const stringData = readFileSync(filePath).toString();
   const data = JSON.parse(stringData);
   data.bundleDependencies = Object.keys(data.dependencies);
   writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
 
-updateBundleDependencies(process.argv.slice(2));
+updateBundleDependencies();
